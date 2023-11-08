5c55d0cb4c8b710e75e4237d90117ec9db03fbdb
Ciornei Daniel <101406950+amseiden@users.noreply.github.com>
2023-11-08 15:47:51 +0200

    Add lasts.

---
+++ 1_Control_5/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Exersaare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a series of numbers separated by comma: ");
+            string userInput = Console.ReadLine();
+
+            string[] myNumbers = userInput.Split(',');
+            List<double> userNumbers = new List<double>();
+
+            foreach(string numString in myNumbers)
+            {
+                if (double.TryParse(numString, out double number))
+                    userNumbers.Add(number);
+                else
+                    Console.WriteLine("Invalid input");
+            }
+
+            double maxNumber = userNumbers.Count > 0 ? userNumbers.Max() : 0;
+            Console.WriteLine($"Maximum number entered: {maxNumber}");
+            
+            string[] myumbers = userInput.Split();
+            
+        }
+    }
+}
