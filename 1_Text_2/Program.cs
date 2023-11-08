eb100f5e731dc396f27778e0045acc5482464e8a
Ciornei Daniel <101406950+amseiden@users.noreply.github.com>
2023-11-08 14:56:41 +0200

    Add project 1_Text_2
    First drag-and-drop add.

---
+++ 1_Text_2/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * Write a program and ask the user to enter a few numbers separated by a hyphen.
+ * If the user simply presses Enter, without supplying an input, exit immediately;
+ * otherwise, check to see if there are duplicates. If so, display "Duplicate" on the console.
+ */
+
+namespace Text
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //arunc pe git asta 
+                        
+        }
+    }
+}
